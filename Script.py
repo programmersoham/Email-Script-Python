884330ea32f0b6424288035831c7738525871a2e
soham117 <purohit.soham.ps@gmail.com>
2022-12-29 10:30:30 +0530

    Initial Commit

---
+++ Script.py
@@ -0,0 +1,31 @@
+# Python Script to Automate email with attachments and multiple recepeients
+import os
+import smtplib
+from email.message import EmailMessage
+
+email_id = 'sender@gmail.com'  # your email here
+email_pass = 'Enter your password here'  # your password here
+
+
+recipient_list = ['recepient1@gmail.com',
+                  'recepient2@gmail.com', 'recepient3@gmail.com']  # recepient emails here
+msg = EmailMessage()
+msg['Subject'] = 'Enter your Subject line :)'
+msg['From'] = email_id
+msg['To'] = recipient_list
+
+msg.set_content('line1\
+    line2\ line3\n line4')  # email body here
+
+for each_file in os.listdir():
+    if each_file == 'Script.py':
+        continue
+    with open(each_file, 'rb') as f:
+        file_data = f.read()
+        file_name = f.name
+        msg.add_attachment(file_data, maintype='application',
+                           subtype='octet-stream', filename=file_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(email_id, email_pass)
+    smtp.send_message(msg)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# How to use this with gmail as your email provider
+
+- if you are using gmail as your email provider, enter google app password in password field
+- [Click here](https://myaccount.google.com/apppasswords) to generate your app password
+
+## Note: use this script in a sperate directory with your attachments that you want to send
+
+- this script will send all the files as attachment from the directory apart from Script.py file  
